2c6ecdbe6aa24f0548b7541a32e9c00fa5849d0d
LeMoussel <cnhx27@gmail.com>
2023-12-01 11:04:28 +0100

    workflow pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
-import sys
-import os
-import setuptools
-
-#import gpyrobotstxt
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name='gpyrobotstxt',
-    version=gpyrobotstxt.__version__,
+    keywords='gpyrobotstxt',
+    version="1.0.0",
     author="CoconSe",
     author_email="blog@cocon.se",
     license='GPL v3',
@@ -17,8 +14,8 @@ setuptools.setup(
     description="A pure Python port of Google's robots.txt parser and matcher.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    package_dir={"":"gpyrobotstxt"},
-    packages=setuptools.find_packages(where="gpyrobotstxt"),
+    include_package_data=True,
+    packages=find_packages(include=['gpyrobotstxt']),
     install_requires=[
     ],
     python_requires='>=3.9',
